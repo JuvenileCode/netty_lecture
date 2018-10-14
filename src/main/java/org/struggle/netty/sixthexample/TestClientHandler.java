e15aae9a40c60a0a8ede1e34c1d8d6a3b8c19405
Bin.L <csdn.liu.bin@gmail.com>
2018-10-14 13:26:18 +0800

    Protobuf多协议消息

--- src/main/java/org/struggle/netty/sixthexample/TestClientHandler.java
+++ src/main/java/org/struggle/netty/sixthexample/TestClientHandler.java
@@ -4,20 +4,40 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import org.struggle.protobuf.DataInfo;
 
+import java.util.Random;
+
 /**
  * @Auther: Bin.L
  * @Date: 2018/10/13 21:38
  * @Description:
  */
-public class TestClientHandler extends SimpleChannelInboundHandler<DataInfo.Student> {
+public class TestClientHandler extends SimpleChannelInboundHandler<DataInfo.MyMessage> {
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, DataInfo.Student msg) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, DataInfo.MyMessage msg) throws Exception {
 
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        DataInfo.Student student = DataInfo.Student.newBuilder().setName("张三").setAge(22).setAddress("北京").build();
-        ctx.channel().writeAndFlush(student);
+        int randomInt = new Random().nextInt(3);
+        DataInfo.MyMessage myMessage = null;
+        if (0 == randomInt) {
+            myMessage = DataInfo.MyMessage.newBuilder()
+                    .setDataType(DataInfo.MyMessage.DataType.StudentType)
+                    .setStudent(DataInfo.Student.newBuilder().setName("张三").setAge(22).setAddress("北京").build())
+                    .build();
+        } else if (1 == randomInt) {
+            myMessage = DataInfo.MyMessage.newBuilder()
+                    .setDataType(DataInfo.MyMessage.DataType.DogType)
+                    .setDog(DataInfo.Dog.newBuilder().setName("小白").setAge(3).build())
+                    .build();
+        } else {
+            myMessage = DataInfo.MyMessage.newBuilder()
+                    .setDataType(DataInfo.MyMessage.DataType.CatType)
+                    .setCat(DataInfo.Cat.newBuilder().setCity("上海").setName("hah").build())
+                    .build();
+        }
+
+        ctx.channel().writeAndFlush(myMessage);
     }
 }
--- src/main/java/org/struggle/netty/sixthexample/TestServerHandler.java
+++ src/main/java/org/struggle/netty/sixthexample/TestServerHandler.java
@@ -9,9 +9,19 @@ import org.struggle.protobuf.DataInfo;
  * @Date: 2018/10/13 21:24
  * @Description:
  */
-public class TestServerHandler extends SimpleChannelInboundHandler<DataInfo.Student> {
+public class TestServerHandler extends SimpleChannelInboundHandler<DataInfo.MyMessage> {
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, DataInfo.Student msg) throws Exception {
-        System.out.println(msg.getName() + " " + msg.getAge() + " " + msg.getAddress());
+    protected void channelRead0(ChannelHandlerContext ctx, DataInfo.MyMessage msg) throws Exception {
+        DataInfo.MyMessage.DataType dataType = msg.getDataType();
+        if (dataType == DataInfo.MyMessage.DataType.StudentType) {
+            DataInfo.Student student = msg.getStudent();
+            System.out.println(student.getName() + " " + student.getAge() + " " + student.getAddress());
+        } else if (dataType == DataInfo.MyMessage.DataType.DogType) {
+            DataInfo.Dog dog = msg.getDog();
+            System.out.println(dog.getName() + " " + dog.getAge());
+        } else {
+            DataInfo.Cat cat = msg.getCat();
+            System.out.println(cat.getName() + " " + cat.getCity());
+        }
     }
 }
